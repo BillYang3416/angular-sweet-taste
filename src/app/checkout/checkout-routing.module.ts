45f9509936a16772165bd2954d0aaa0ee8ad6b53
SYSTEX\2000160 <2000160@systex.com>
2021-01-21 10:57:53 +0800

    2. 設定路由

--- src/app/checkout/checkout-routing.module.ts
+++ src/app/checkout/checkout-routing.module.ts
@@ -1,8 +1,43 @@
+import { ReceiptInfoComponent } from './receipt-info/receipt-info.component';
+import { PaymentInfoComponent } from './payment-info/payment-info.component';
+import { CustomerInfoComponent } from './customer-info/customer-info.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { appPath } from '../app-path.const';
 
-const routes: Routes = [];
+import { CheckoutComponent } from './checkout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CheckoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: appPath.checkoutFlow.customerInfo,
+        pathMatch: 'full'
+      },
+      {
+        path: appPath.checkoutFlow.customerInfo,
+        component: CustomerInfoComponent,
+      },
+      {
+        path: appPath.checkoutFlow.paymentInfo,
+        component: PaymentInfoComponent
+      },
+      {
+        path: appPath.checkoutFlow.receiptInfo,
+        component: ReceiptInfoComponent,
+      },
+      {
+        path: '**',
+        redirectTo: appPath.checkoutFlow.customerInfo,
+        pathMatch: 'full',
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
