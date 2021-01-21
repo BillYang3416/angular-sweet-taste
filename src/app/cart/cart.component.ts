50379bdb8429f5cde55fc121675955315681a979
SYSTEX\2000160 <2000160@systex.com>
2021-01-21 12:13:15 +0800

    3-7. 購物車切版

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { appPath } from './../app-path.const';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+
+  /** 路由定義 */
+  path = appPath;
 
   constructor() { }
 
