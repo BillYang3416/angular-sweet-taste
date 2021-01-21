c601c921fd5e79dc22e1212a25c0a44131b66856
SYSTEX\2000160 <2000160@systex.com>
2021-01-21 14:32:18 +0800

    3-9. 成功頁面切版

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { appPath } from './../app-path.const';
+
 @Component({
   selector: 'app-success',
   templateUrl: './success.component.html',
   styleUrls: ['./success.component.css']
 })
 export class SuccessComponent implements OnInit {
+
+  /** 路由定義 */
+  path = appPath;
 
   constructor() { }
 
