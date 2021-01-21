696559ef7548420dd7ad2f6dc56cd84f7e62c70f
SYSTEX\2000160 <2000160@systex.com>
2021-01-21 12:03:25 +0800

    3-6. 首頁切版

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { appPath } from '../app-path.const';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  /** 路由定義 */
+  path = appPath;
 
   constructor() { }
 
